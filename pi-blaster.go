5b6e2f57935f6d165377186442429273a2c495d1
Dave Drager <ddrager@gmail.com>
2014-12-03 01:45:26 +0000

    Learning golang is fun...

--- pi-blaster.go
+++ pi-blaster.go
@@ -18,9 +18,9 @@ func check(e error) {
   }
 }
 
-func Start(start []int64) {
+func (b *Blaster) Start(start []int64) {
 
-  var b Blaster 
+  //var b Blaster 
   const fifo = "/dev/pi-blaster"
 
   copy(start, b.active)
@@ -36,6 +36,7 @@ func Start(start []int64) {
   for i := range b.active {
     fmt.Printf("Set pin %d to 0\n", b.active[i])
     f.WriteString(strconv.FormatInt(b.active[i], 10) + "=0\n") 
+    b.Pins[b.active[i]-1] = 0.0
     f.Sync()
     //check(err)
   }
@@ -43,13 +44,21 @@ func Start(start []int64) {
   f.Close()
 }
 
-func Apply(pin int64, value int64) {
+func (b *Blaster) Apply(pin int64, value float64) {
   f, err := os.Create("/dev/pi-blaster")
   check(err)
   defer f.Close()
-
-  n1, err := f.WriteString(strconv.FormatInt(pin, 10) + "=" + strconv.FormatInt(value, 10) + "\n")
-  fmt.Printf("wrote %d bytes (%d = %d)\n", n1, pin, value)
+  var toVal string
+  toVal = strconv.FormatFloat(value, 'f', 2, 64)
+  n1, err := f.WriteString(strconv.FormatInt(pin, 10) + "=" + toVal + "\n")
+  b.Pins[pin-1] = value
+  fmt.Printf("wrote %d bytes (%d = %s)\n", n1, pin, toVal)
   f.Sync()
+}
 
+func (b *Blaster) DumpCurrent() {
+  fmt.Printf("Dumping Current Values...\n")
+  for i := range b.active {
+    fmt.Printf("%d = %f\n", b.active[i], b.Pins[b.active[i]-1]) 
+  }
 }
