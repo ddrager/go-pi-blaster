609f01fedcc919e8fcaaca08f1ab0ec6e1ef18d4
ddrager <ddrager@gmail.com>
2016-12-01 14:03:48 -0500

    Adds Reset function to reset all pins to 0, removes debugging output

--- pi-blaster.go
+++ pi-blaster.go
@@ -35,7 +35,7 @@ func (b *Blaster) Start(start []int64) {
 
   // set all active pin values to 0
   for i := range b.active {
-    fmt.Printf("Set pin %d to 0\n", b.active[i])
+    // fmt.Printf("Set pin %d to 0\n", b.active[i])
     f.WriteString(strconv.FormatInt(b.active[i], 10) + "=0\n") 
     b.Pins[b.active[i]-1] = 0.0
     f.Sync()
@@ -61,9 +61,10 @@ func (b *Blaster) Apply(pin int64, value float64) {
 
   var toVal string
   toVal = strconv.FormatFloat(value, 'f', 2, 64)
-  n1, err := f.WriteString(strconv.FormatInt(pin, 10) + "=" + toVal + "\n")
+  f.WriteString(strconv.FormatInt(pin, 10) + "=" + toVal + "\n")
+  // n1, err := f.WriteString(strconv.FormatInt(pin, 10) + "=" + toVal + "\n")
   b.Pins[pin-1] = value
-  fmt.Printf("wrote %d bytes (%d = %s)\n", n1, pin, toVal)
+  // fmt.Printf("wrote %d bytes (%d = %s)\n", n1, pin, toVal)
   f.Sync()
 }
 
@@ -73,3 +74,31 @@ func (b *Blaster) DumpCurrent() {
     fmt.Printf("%d = %f\n", b.active[i], b.Pins[b.active[i]-1]) 
   }
 }
+
+func (b *Blaster) Reset() {
+  fmt.Printf("Resetting Pins\n")
+  //TODO: Ensure we are running pi and have pi-blaster running
+
+  const fifo = "/dev/pi-blaster"
+
+  b.active = make([]int64, 0)
+  // initialize array used to keep track of pin value on updates
+  b.Pins = make([]float64, 26)
+
+  f, err := os.Create(fifo)
+  check(err)
+  defer f.Close()
+
+  // set all active pin values to 0
+  for i := range b.active {
+    // fmt.Printf("Set pin %d to 0\n", b.active[i])
+    f.WriteString(strconv.FormatInt(b.active[i], 10) + "=0\n") 
+    b.Pins[b.active[i]-1] = 0.0
+    f.Sync()
+    //check(err)
+  }
+
+  f.Close()
+
+}
+
